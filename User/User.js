58feafff5319d593893157df9324527f1ce76dac
bhautik-baraiya <bhautikbaraiya126@gmail.com>
2024-08-09 09:40:47 +0530

    second commit

--- User/User.js
+++ User/User.js
@@ -4,7 +4,7 @@ const User = require('../Model/User/User');
 
 // Get All User Router 
 
-Router.get('/users/', async (req, res) => {
+Router.get('/users', async (req, res) => {
     try {
         const users = await User.find();
         res.status(201).json(users);
@@ -13,7 +13,7 @@ Router.get('/users/', async (req, res) => {
     }
 })
 
-Router.post('/users/', (req, res) => {
+Router.post('/users', (req, res) => {
     try {
         const user = new User(req.body);
         const savedUser = user.save();
